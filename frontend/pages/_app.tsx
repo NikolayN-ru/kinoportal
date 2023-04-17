c5696ede50022d5279c29a9830191ce6503809b4
NikolayN-ru <napadaylonikolay@gmail.com>
2023-04-17 13:49:47 +0400

    page actor

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,15 @@
-import '../styles/globals.scss';
-import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { store } from '../redux/store';
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
+import MainContainer from "../components/MainContainer/MainContainer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <MainContainer>
+        <Component {...pageProps} />
+      </MainContainer>
     </Provider>
   );
 }
