06551142ddd309a4a3a421bc0ffadd3c209a7a7a
Maxim Butenko <98466211+maxbuten97@users.noreply.github.com>
2023-04-22 20:28:15 +0300

    corrected files

--- frontend/pages/admin/index.tsx
+++ frontend/pages/admin/index.tsx
@@ -1,5 +1,27 @@
-const index = () => {
-  return <div>adminka</div>;
+import GrayButton from "../../components/GrayButton";
+import Icon from "../../components/shared/IconComponent/Icon";
+import s from "./home/HomeAdmin.module.scss";
+import ControlsBar from "../../componentsAdmin/ControlsBar/ControlsBar";
+import Navbar from "../../componentsAdmin/Navbar/Navbar";
+
+const HomeAdmin = () => {
+  return (
+    <div className={s.HomeAdmin}>
+      <Navbar />
+      <div className={s.row}>
+        <div className={s.header}>
+          <div className={s.title}>Главная</div>
+          <div className={s.header_btns}>
+            <GrayButton className={s.add_btn} title="Добавить +" />
+            <div className={s.search_btn}>
+              <Icon name="search" />
+            </div>
+          </div>
+        </div>
+        <ControlsBar />
+      </div>
+    </div>
+  );
 };
 
-export default index;
+export default HomeAdmin;
