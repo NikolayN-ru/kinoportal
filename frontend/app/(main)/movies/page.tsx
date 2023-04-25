cc7103a8f7d364e05b2fe3756d804ef59f96f16f
Михаил <72045008+meatrue@users.noreply.github.com>
2023-04-25 21:27:37 +0300

    FRONTEND | PromoSlider + CollectionSlider + CompillationSlider + Tooltip rework (#18)
    * PromoSlider + CollectionSlider + CompillationSlider + Tooltip rework
    
    * Delete package-lock.json
    
    * Update .gitignore
    
    ---------
    
    Co-authored-by: Nikolay <67255397+NikolayN-ru@users.noreply.github.com>

--- frontend/app/(main)/movies/page.tsx
+++ frontend/app/(main)/movies/page.tsx
@@ -1,9 +1,10 @@
-import MainContainer from "@components/MainContainer/MainContainer";
+import MainContainer from '@components/MainContainer/MainContainer';
+import Title from '@components/Title/Title';
 
 export default function Home() {
   return (
     <MainContainer>
-      <h1></h1>
+      <Title tag="h1" size="lg" text="Фильмы смотреть онлайн" />
     </MainContainer>
   );
 }
