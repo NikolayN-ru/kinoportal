b6ec3ba6aa07d14b3c3fb860690fbe97f62e4d67
Siesta <Siesta@DESKTOP-UIOGSB1>
2023-04-27 07:46:05 +0300

    app-gateway

---
+++ backend/app-gateway/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ActorController } from './actor/actor.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { FilmController } from './film/film.controller';
+import { AdminController } from './admin/admin.controller';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'Actor',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'actor-queue',
+          queueOptions: {
+            durable: false
+          },
+        },
+      }])
+  ],
+  controllers: [ActorController,
+    FilmController,
+    AdminController],
+  providers: [],
+})
+export class AppModule {}
