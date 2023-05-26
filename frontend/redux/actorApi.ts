766a96b17a961f465a6476abdc85726883e98c35
Nikolay <67255397+NikolayN-ru@users.noreply.github.com>
2023-05-26 13:17:25 +0400

    query

--- frontend/redux/actorApi.ts
+++ frontend/redux/actorApi.ts
@@ -4,10 +4,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const allActors = createApi({
   reducerPath: "allActors",
-  baseQuery: fetchBaseQuery({ baseUrl: `${process.env.DB_HOST}/actor` }),
+  // baseQuery: fetchBaseQuery({ baseUrl: `${process.env.DB_HOST}/actor` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:4000/actor` }),
   endpoints: (builder) => ({
     allActors: builder.query<any, string>({
-      query: () => ``,
+      query: (all='all') => `/${all}`,
     }),
     actorItem: builder.query<any, string>({
       query: (id) => `/${id}`,
